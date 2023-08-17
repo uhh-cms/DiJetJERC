61de9f0dfcccf4913d485575d7f62fe67d8450fe
apaasch <alexanderpaa94@gmail.com>
2023-08-17 16:12:35 +0200

    Remove prints; lint

--- dijet/scripts/framework_comparison.py
+++ dijet/scripts/framework_comparison.py
@@ -6,7 +6,9 @@ import uproot
 import pickle
 import hist
 
-cf_store_local = "/nfs/dust/cms/user/paaschal/WorkingArea/Analysis/JERC/DiJet/data/dijet_store/"  # $CF_STORE_LOCAL
+cf_user = "/nfs/dust/cms/user/paaschal/WorkingArea/Analysis/JERC/DiJet/"
+cf_store_local = cf_user + "data/dijet_store/"  # $CF_STORE_LOCAL
+uhh_path = "/nfs/dust/cms/user/paaschal/UHH2_DiJet/CMSSW_10_6_28/src/UHH2/DiJetJERC/JERSF_Analysis/"
 
 # arguments
 parser = argparse.ArgumentParser()
@@ -18,12 +20,15 @@ version = args.version
 task = args.task
 category = args.category
 
+
 def create_empty_histogram_like(existing_hist):
     # Create an empty histogram with the same shape
     return hist.Hist(*existing_hist.axes)
 
+
 def cf_get_data():
-    output_path = cf_store_local+"/analysis_dijet/cf.MergeHistograms/config_2017_limited/data_jetht_e/nominal/calib__skip_jecunc/sel__default/prod__default/v0/"
+    output_path = cf_store_local + "/analysis_dijet/cf.MergeHistograms/config_2017_limited/"
+    output_path += "data_jetht_e/nominal/calib__skip_jecunc/sel__default/prod__default/v0/"
 
     file_name = "hist__dijets_asymmetry.pickle"
     pickle_file = os.path.join(output_path, file_name)
@@ -47,10 +52,9 @@ def cf_get_data():
 
 def root_get_data(year, study, corr, coll, run, hname):
     fname = "histograms_data_incl_full.root"
-    dpath = "/nfs/dust/cms/user/paaschal/UHH2_DiJet/CMSSW_10_6_28/src/UHH2/DiJetJERC/JERSF_Analysis/"
     hpath = "hist_preparation/data/wide_eta_bin/file/"
     fpath = f"{study}/{year}/{corr}/{coll}/{run}_{year}/"
-    path = dpath + hpath + fpath + fname
+    path = uhh_path + hpath + fpath + fname
     with uproot.open(path) as uhh2_file:
         # keys = uhh2_file.keys()
         # for key in keys:
@@ -64,33 +68,10 @@ if __name__ == "__main__":
 
     cf_tt = cf_get_data()
     cf_x = cf_tt.axes[3].edges
-    print(11111)
-    print()
-    print(cf_tt.axes)
-    print()
-    print("------------------  NULLER")
-    print(cf_tt.axes[0])
-    print()
-    print(cf_tt.axes[0].edges)
-    print()
-    print("------------------  Threeer")
-    print(cf_tt.axes[3])
-    print()
-    print(cf_tt.axes[3].edges)
-    print()
-    print("------------------  XXXXER")
-    print(cf_x)
-    print(len(cf_x))
     cf_y = cf_tt.values()
     cf_y = cf_y[-1, -1, -1]
-    print("------------------  WHYYYYER")
-    print(cf_y)
-    print(len(cf_y))
     cf_bin_centers = (cf_x[:-1] + cf_x[1:]) / 2
-    print()
-    print(cf_bin_centers)
-    print(len(cf_bin_centers))
-    print(f"cf: {len(cf_x)} x {len(cf_y)}")
+    print(f"cf: {len(cf_bin_centers)} x {len(cf_y)}")
 
     study = "eta_common_fine1_finealpha_prescale"
     hname = "asymm_FE_reference_eta2_pt10_alpha11"
@@ -111,6 +92,6 @@ if __name__ == "__main__":
     ax_root.set_title("Just give me a title")
     ax_root.set_xlabel("jet pt")
     ax_root.set_ylabel("event counts")
-    fig_root.savefig("/nfs/dust/cms/user/paaschal/WorkingArea/Analysis/JERC/DiJet/dijet/scripts/plots/comparison/dijet_asym.pdf")
+    fig_root.savefig(cf_user + "dijet/scripts/plots/comparison/dijet_asym.pdf")
     fig_root.clear()
     print("Created plots for jet1_pt")
