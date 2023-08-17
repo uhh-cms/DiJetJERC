43ee387bdb4ef3317fa3f7cb3b956214b417dd36
apaasch <alexanderpaa94@gmail.com>
2023-08-17 15:55:52 +0200

    Add PlotVariable task

--- dijet/scripts/dijet_tasks.sh
+++ dijet/scripts/dijet_tasks.sh
@@ -3,3 +3,7 @@
 dijet_selection(){
     law run cf.SelectEvents --config config_2017_limited --version v0 --dataset data_jetht_e --remove-output 0,a,y
 }
+
+dijet_plotting(){
+    law run cf.PlotVariables1D --config config_2017_limited --version v0 --dataset data_jetht_e --remove-output 2,a,y --variables dijets_asymmetry
+}
