cfa2131919bea0e36e0afea20ba2818e6c0887d8
apaasch <alexanderpaa94@gmail.com>
2023-12-12 18:06:50 +0100

    remove alpha from categories

--- dijet/config/categories.py
+++ dijet/config/categories.py
@@ -6,11 +6,11 @@ Definition of categories.
 Categories are assigned a unique integer ID according to a fixed numbering
 scheme, with digits/groups of digits indicating the different category groups:
 
-        lowest digit
-                  |
-    +---+---+---+---+
-    | I | A | A | M |
-    +---+---+---+---+
+    lowest digit
+      |
+    +---+
+    | M |
+    +---+
 
 +=======+===============+======================+=================================+
 | Digit | Description   | Values               | Category name                   |
@@ -28,17 +28,6 @@ scheme, with digits/groups of digits indicating the different category groups:
 |       |               |                      |    (SM flag must be false)      |
 |       |               |                      |    JER measured from probe jet  |
 | x 1   |               |                      |    JER from FE dependent on SM  |
-+-------+---------------+----------------------+---------------------------------+
-| A     | alpha binning | 1 to a               | alpha_{min}_{max} (exclusive)   |
-| x 100 |               |                      | or                              |
-+-------+---------------+----------------------+ alpha_lt_{max} (inclusive)      |
-| I     | Inclusive     |                      |                                 |
-|       | alpha flag    |                      | In combination with alpha bins  |
-|       |               | 0: exclusive         | Excl: alpha in one bin          |
-|       |               | 1: inclusive         | Incl: All alpha below the upper |
-|       |               |                      |       bin value                 |
-|       |               |                      | e.g.  bin IA=06 -> 0.25<a<0.3   |
-| x 1e3 |               |                      |       bin IA=16 ->      a<0.3   |
 +-------+---------------+----------------------+---------------------------------+
 
 A digit group consisting entirely of zeroes ('0') represents the inclusive
@@ -60,7 +49,6 @@ from columnflow.categorization import Categorizer, categorizer
 
 from dijet.production.dijet_balance import dijet_balance
 from dijet.production.jet_assignment import jet_assignment
-from dijet.constants import alpha
 
 import order as od
 
@@ -90,8 +78,6 @@ def skip_fn(categories: dict[str, od.Category]):
     """Custom function for skipping certain category combinations."""
     # skip if combination involves both `alpha` and `alpha_incl` groups,
     # since these are not disjoint
-    if all(group in categories for group in ["alpha", "alpha_incl"]):
-        return True
 
     return False  # don't skip
 
@@ -163,139 +149,3 @@ def add_categories(config: od.Config) -> None:
         label="fe",
     )
     method_categories.append(cat)
-
-    #
-    # group 2: alpha bins
-    #
-
-    cat_idx_lsd += cat_idx_ndigits  # 10-power of least significant digit
-    cat_idx_ndigits = 2  # number of digits to use for category group
-
-    # get alpha bins from config
-    alpha_bins = alpha  # TODO: Add binning in config ?
-    alpha_categories = []
-
-    for cat_idx, (alpha_min, alpha_max) in enumerate(
-        zip(alpha_bins[:-1], alpha_bins[1:]),
-    ):
-        alpha_min_repr = f"{alpha_min}".replace(".", "p")
-        alpha_max_repr = f"{alpha_max}".replace(".", "p")
-        cat_label = rf"{alpha_min} $\leq$ $\alpha$ < {alpha_max}"
-
-        cat_name = f"alpha_{alpha_min_repr}_{alpha_max_repr}"
-        sel_name = f"sel_{cat_name}"
-
-        @categorizer(
-            uses={dijet_balance},
-            cls_name=sel_name,
-        )
-        def sel_alpha(
-            self: Categorizer, events: ak.Array,
-            alpha_range: tuple = (alpha_min, alpha_max),
-            alpha_min_repr=alpha_min_repr,
-            alpha_max_repr=alpha_max_repr,
-            **kwargs,
-        ) -> ak.Array:
-            f"""
-            Select events with probe jet alpha the range [{alpha_min_repr}, {alpha_max_repr})
-            """
-            events = self[dijet_balance](events, **kwargs)
-            return events, ak.fill_none(
-                (events.dijets.alpha >= alpha_range[0]) &
-                (events.dijets.alpha < alpha_range[1]),
-                False,
-            )
-
-        assert cat_idx < 10**cat_idx_ndigits - 1, "no space for category, ID reassignement necessary"
-        cat = config.add_category(
-            name=cat_name,
-            id=int(10**cat_idx_lsd * (cat_idx + 1)),
-            selection=sel_name,
-            label=cat_label,
-        )
-        alpha_categories.append(cat)
-
-    #
-    # group 3: inclusive alpha categories from union of (exclusive) alpha bins
-    #
-
-    cat_idx_lsd += cat_idx_ndigits
-    cat_idx_ndigits = 1
-    alpha_incl_bins = [
-        f"lt_{str(a).replace('.', 'p')}" for a in alpha[1:]
-    ]
-    assert len(alpha_incl_bins) + 1 == len(alpha)
-    alpha_incl_categories = []
-    for cat_idx, (alpha_bin, alpha_val) in enumerate(zip(alpha_incl_bins, alpha_bins[1:])):
-        cat_label = rf"$\alpha$ < {alpha_val}"
-
-        cat_name = f"alpha_{alpha_bin}"
-        sel_name = f"sel_{cat_name}"
-
-        # create category and add individual alpha intervals as child categories
-        cat = config.add_category(
-            name=cat_name,
-            id=int(10**cat_idx_ndigits * (cat_idx + 1) + 10**cat_idx_lsd),
-            selection=None,
-            label=cat_label,
-        )
-        child_cats = alpha_categories[:cat_idx + 1]
-        for child_cat in child_cats:
-            cat.add_category(child_cat)
-
-        alpha_incl_categories.append(cat)
-
-    # -- combined categories
-
-    def add_combined_categories(config):
-        if getattr(config, "has_combined_categories", False):
-            return  # combined categories already added
-
-        category_groups = {
-            "method": method_categories,
-            "alpha": alpha_categories,
-            "alpha_incl": alpha_incl_categories,
-        }
-
-        create_category_combinations(
-            config,
-            category_groups,
-            name_fn,
-            kwargs_fn,
-            skip_fn=skip_fn,
-            skip_existing=False,
-        )
-
-        # connect intermediary `alpha_incl` and `alpha` categories
-        category_groups_no_alpha = {
-            "method": method_categories,
-        }
-
-        # go through all possible combinations of category *groups*
-        for n in range(1, len(category_groups_no_alpha) + 1):
-            for group_names in itertools.combinations(category_groups_no_alpha, n):
-                # get root categories for each group
-                root_cats = [category_groups_no_alpha[gn] for gn in group_names]
-                # go through cross product of *root categories* (one per group)
-                for root_cats in itertools.product(*root_cats):
-                    root_cats = dict(zip(group_names, root_cats))
-                    # go through non-compound inclusive alpha
-                    # categories and their children
-                    for alpha_incl_cat in alpha_incl_categories:
-                        root_cats_1 = dict(root_cats, **{"alpha_incl": alpha_incl_cat})
-                        name_1 = name_fn(root_cats_1)
-                        cat_1 = config.get_category(name_1)
-                        for alpha_cat in alpha_incl_cat.categories:
-                            # skip compound children
-                            if "__" in alpha_cat.name:
-                                continue
-                            root_cats_2 = dict(root_cats, **{"alpha": alpha_cat})
-                            name_2 = name_fn(root_cats_2)
-                            cat_2 = config.get_category(name_2)
-                            # add parent-child relationship between compound
-                            # alpha inclusive and exclusive categories
-                            cat_1.add_category(cat_2)
-
-        config.has_combined_categories = True
-
-    add_combined_categories(config)
