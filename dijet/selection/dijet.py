e89e95af20a7def64944229f6809c4433362e376
apaasch <alexanderpaa94@gmail.com>
2023-12-15 15:29:39 +0100

    Correct definition of delta phi cut

--- dijet/selection/dijet.py
+++ dijet/selection/dijet.py
@@ -3,6 +3,7 @@
 from typing import Tuple
 from columnflow.util import maybe_import
 from columnflow.selection import Selector, SelectionResult, selector
+from dijet.production.jet_assignment import jet_assignment
 
 ak = maybe_import("awkward")
 np = maybe_import("numpy")
@@ -10,9 +11,7 @@ np = maybe_import("numpy")
 
 @selector(
     uses={
-        "use_sm", "use_fe",
-        "probe_jet.pt", "probe_jet.eta", "probe_jet.phi", "probe_jet.mass",
-        "reference_jet.pt", "reference_jet.eta", "reference_jet.phi", "reference_jet.mass",
+        jet_assignment,
     },
     exposed=True,
 )
@@ -28,27 +27,28 @@ def dijet_selection(
 
     # dijet
 
+    events = self[jet_assignment](events, **kwargs)
+
     # two back-to-back leading jets (delta_phi(j1,j2) = min(|phi1 - phi2|, 2PI - |phi2 - phi1|) > 2.7)
     # copied from https://root.cern.ch/doc/master/TVector2_8cxx_source.html#l00103
     # TODO: LorentzVector behavior delta_phi from coffea
-    pi = np.pi
-    delta_phi = events.reference_jet.phi - events.probe_jet.phi
+    delta_phi = events.probe_jet.phi - events.reference_jet.phi
     delta_phi_pi = ak.where(
-        delta_phi >= pi,  # condition
-        delta_phi - 2 * pi,  # if yes
+        delta_phi >= np.pi,  # condition
+        delta_phi - 2 * np.pi,  # if yes
         ak.where(  # else
-            delta_phi < ((-1) * pi),
-            delta_phi + 2 * pi,
+            delta_phi < ((-1) * np.pi),
+            delta_phi + 2 * np.pi,
             delta_phi,
         ),
     )
 
     dijet_sel = (
         (
-            (abs(events.probe_jet.pt) > 15) &
+            (abs(events.probe_jet.pt) > 15) |
             (abs(events.reference_jet.pt) > 15)
         ) &
-        (delta_phi_pi > 2.7)
+        (abs(delta_phi_pi) > 2.7)
     )
 
     dijet_sel = ak.fill_none(dijet_sel, False)
