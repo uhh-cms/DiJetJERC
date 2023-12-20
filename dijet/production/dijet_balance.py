bbb1185b8d2acc08e6dc3968397ec5a388fb6ee0
Yannick Fischer <yannick.fischer@cern.ch>
2023-12-20 10:42:00 +0100

    Filled Gen Level MPF /Response

--- dijet/production/dijet_balance.py
+++ dijet/production/dijet_balance.py
@@ -28,29 +28,60 @@ Creates column 'Dijet', which includes the most relevant properties of the JetME
         "MET.pt", "MET.phi",
     },
     produces={
-        "dijets.pt_avg", "dijets.asymmetry", "dijets.alpha", "dijets.mpf", "dijets.mpfx",
+        "dijets.pt_avg", "dijets.asymmetry", "dijets.alpha", "dijets.mpf", "dijets.mpfx"
     },
 )
 def dijet_balance(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
 
     # TODO: for now, this only works for reco level
     jets = events.Jet
+    if self.dataset_inst.is_mc:
+        genJets = events.GenJet
     jets = ak.pad_none(jets, 3)
 
     pt_avg = (events.probe_jet.pt + events.reference_jet.pt) / 2
+    if self.dataset_inst.is_mc:
+        probe_genJet_pt = np.array([all_genJets[index] for all_genJets, index in zip(genJets.pt, events.probe_jet.genJetIdx)])
+        reference_genJet_pt = np.array([all_genJets[index] for all_genJets, index in zip(genJets.pt, events.reference_jet.genJetIdx)])
+        probe_genJet_phi = np.array([all_genJets[index] for all_genJets, index in zip(genJets.phi, events.probe_jet.genJetIdx)])
+        reference_genJet_phi = np.array([all_genJets[index] for all_genJets, index in zip(genJets.phi, events.reference_jet.genJetIdx)])
+        response_probe = ak.where(events.probe_jet.genJetIdx != -1,probe_genJet_pt / events.probe_jet.pt,-100)
+        response_reference = ak.where(events.reference_jet.genJetIdx != -1, reference_genJet_pt / events.reference_jet.pt, -100)
+        pt_avg_gen =  ak.where((events.reference_jet.genJetIdx != -1) & (events.probe_jet.genJetIdx != -1), np.divide((probe_genJet_pt + reference_genJet_pt), 2) , -100)
+        mpf_gen =  ak.where((events.reference_jet.genJetIdx != -1) & (events.probe_jet.genJetIdx != -1), np.divide(events.GenMET.pt * np.cos(probe_genJet_phi - events.GenMET.phi),(2 * pt_avg_gen)), -100)
+        mpfx_gen =  ak.where((events.reference_jet.genJetIdx != -1) & (events.probe_jet.genJetIdx != -1), np.divide(events.GenMET.pt * np.sin(probe_genJet_phi - events.GenMET.phi),(2 * pt_avg_gen)), -100)
     asym = (events.probe_jet.pt - events.reference_jet.pt) / (2 * pt_avg)
     alpha = jets.pt[:, 2] / pt_avg
     delta_phi = events.probe_jet.phi - events.MET.phi
     mpf = events.MET.pt * np.cos(delta_phi) / (2 * pt_avg)
     mpfx = events.MET.pt * np.sin(delta_phi) / (2 * pt_avg)
-
-    dijets = ak.zip({
-        "pt_avg": pt_avg,
-        "asymmetry": asym,
-        "alpha": alpha,
-        "mpf": mpf,
-        "mpfx": mpfx,
-    })
+    if self.dataset_inst.is_mc:
+        dijets = ak.zip({
+            "pt_avg": pt_avg,
+            "response_reference": response_reference,
+            "response_probe": response_probe,
+            "asymmetry": asym,
+            "alpha": alpha,
+            "mpf": mpf,
+            "mpfx": mpfx,
+            "mpf_gen": mpf_gen,
+            "mpfx_gen": mpfx_gen,
+            "pt_avg_gen": pt_avg_gen,
+        })
+    else:
+        dijets = ak.zip({
+            "pt_avg": pt_avg,
+            "asymmetry": asym,
+            "alpha": alpha,
+            "mpf": mpf,
+            "mpfx": mpfx,
+        })
     events = set_ak_column(events, "dijets", dijets)
 
     return events
+
+@dijet_balance.init
+def dijet_balance_init(self: Producer) -> None:
+     if self.dataset_inst.is_mc:
+        self.uses |= {"GenJet.pt","GenJet.phi", "probe_jet.genJetIdx", "reference_jet.genJetIdx","GenMET.pt","GenMET.phi",}
+        self.produces |= {"dijets.response_probe", "dijets.response_reference", "dijets.mpf_gen", "dijets.mpfx_gen", "dijets.pt_avg_gen",}
