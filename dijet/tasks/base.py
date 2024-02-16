5590f9e24d814747d0b92c2d2e795b2c8dd69d8d
apaasch <alexanderpaa94@gmail.com>
2024-02-16 14:52:52 +0100

    Remove SelectorStepsMixin

--- dijet/tasks/base.py
+++ dijet/tasks/base.py
@@ -9,7 +9,7 @@ import law
 
 from columnflow.tasks.framework.base import BaseTask, ShiftTask
 from columnflow.tasks.framework.mixins import (
-    CalibratorsMixin, SelectorStepsMixin, ProducersMixin,
+    CalibratorsMixin, ProducersMixin,
     VariablesMixin, DatasetsProcessesMixin, CategoriesMixin,
 )
 from columnflow.config_util import get_datasets_from_process
@@ -26,7 +26,6 @@ class HistogramsBaseTask(
     CategoriesMixin,
     VariablesMixin,
     ProducersMixin,
-    SelectorStepsMixin,
     CalibratorsMixin,
     ShiftTask,
     law.LocalWorkflow,
