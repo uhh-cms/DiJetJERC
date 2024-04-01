714f63941082262ef643aeff98a69e77f6f745a9
apaasch <alexanderpaa94@gmail.com>
2024-04-01 14:54:41 +0200

    remove util functions from task space

--- dijet/tasks/plot_base.py
+++ dijet/tasks/plot_base.py
@@ -76,24 +76,3 @@ class PlottingBaseTask(
         parts = super().store_parts()
         return parts
 
-    def eta_bin(self, eta_lo, eta_hi):
-        return fr"{eta_lo}<$\eta$<{eta_hi}"
-
-    def pt_bin(self, pt_lo, pt_hi):
-        spt = r"$p_\mathrm{T}$"
-        return fr"{pt_lo}<{spt}<{pt_hi}"
-
-    def alpha_bin(self, alpha):
-        salpha = r"$\alpha$<"
-        return fr"{salpha}{alpha}"
-
-    def dot_to_p(self, var):
-        return f"{var}".replace(".", "p")
-
-    def add_text(self, ax, x, y, text, offset=0, ho="left", ve="top"):
-        ax.text(
-            x, y - offset, text,
-            transform=ax.transAxes,
-            horizontalalignment=ho,
-            verticalalignment=ve,
-        )
