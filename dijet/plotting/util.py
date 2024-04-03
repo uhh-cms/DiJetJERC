71c28ef9f9497b26193d58484ff9f470f69415da
apaasch <alexanderpaa94@gmail.com>
2024-04-03 11:44:18 +0200

    remove self argument from util functions

--- dijet/plotting/util.py
+++ dijet/plotting/util.py
@@ -1,20 +1,20 @@
 # coding: utf-8
 
-def eta_bin(self, eta_lo, eta_hi):
+def eta_bin(eta_lo, eta_hi):
     return fr"{eta_lo}<$\eta$<{eta_hi}"
                                                                    
-def pt_bin(self, pt_lo, pt_hi):
+def pt_bin(pt_lo, pt_hi):
     spt = r"$p_\mathrm{T}$"
     return fr"{pt_lo}<{spt}<{pt_hi}"
                                                                    
-def alpha_bin(self, alpha):
+def alpha_bin(alpha):
     salpha = r"$\alpha$<"
     return fr"{salpha}{alpha}"
                                                                    
-def dot_to_p(self, var):
+def dot_to_p(var):
     return f"{var}".replace(".", "p")
                                                                    
-def add_text(self, ax, x, y, text, offset=0, ha="left", va="top"):
+def add_text(ax, x, y, text, offset=0, ha="left", va="top"):
     ax.text(
         x, y - offset, text,
         transform=ax.transAxes,
