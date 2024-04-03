8e94eb088503569c155b890fb1cc0c2f50c9264e
apaasch <alexanderpaa94@gmail.com>
2024-04-03 20:56:25 +0200

    add plotting for jer

--- dijet/plotting/jer.py
+++ dijet/plotting/jer.py
@@ -28,6 +28,11 @@ class PlotJERs(PlottingBaseTask):
         JER=JER,
     )
 
+    colors = {
+        "da": "black",
+        "mc": "indianred",
+    }
+
     def requires(self):
         return self.reqs.JER.req(
             self,
@@ -48,9 +53,20 @@ class PlotJERs(PlottingBaseTask):
         target = self.target(f"{sample}", dir=True)
         # declare the main target
         outp = {
+            "single": target.child("jers", type="d"),
             "dummy": target.child("dummy.txt", type="f"),
         }
         return outp
+
+    def plot_jers(self, data, mc, pt):
+        fig, ax = plt.subplots()
+
+        plt.errorbar(pt, mc["nom"], yerr=mc["err"], fmt="o", color=self.colors["mc"], label="MC")
+        plt.errorbar(pt, data["nom"], yerr=data["err"], fmt="o", color=self.colors["da"], label="Data")
+
+        ax.set_xlabel(r"$p_{T}^{ave}$")
+        ax.set_ylabel(r"$JER$")
+        return fig, ax
 
     def run(self):
         jers_da, jers_mc = self.load_jers()
@@ -63,9 +79,49 @@ class PlotJERs(PlottingBaseTask):
         jers_mc.view().variance = np.nan_to_num(jers_mc.view().variance, nan=0.0)
 
         eta_edges = jers_da.axes["probejet_abseta"].edges
+        pt_centers = jers_da.axes["dijets_pt_avg"].centers
+
+        # Set plotting style
+        plt.style.use(mplhep.style.CMS)
+
+        pos_x = 0.05
+        pos_y = 0.95
 
         for m in self.LOOKUP_CATEGORY_ID:
             for ie, (eta_lo, eta_hi) in enumerate(zip(eta_edges[:-1], eta_edges[1:])):
 
+                input_ = {
+                    "data": {
+                        "nom": jers_da[hist.loc(self.LOOKUP_CATEGORY_ID[m]), ie, :].values(),
+                        "err": jers_da[hist.loc(self.LOOKUP_CATEGORY_ID[m]), ie, :].variances(),
+                    },
+                    "mc": {
+                        "nom": jers_mc[hist.loc(self.LOOKUP_CATEGORY_ID[m]), ie, :].values(),
+                        "err": jers_mc[hist.loc(self.LOOKUP_CATEGORY_ID[m]), ie, :].variances(),
+                    },
+                    "pt": pt_centers,
+                }
+
+                fig, ax = self.plot_jers(**input_)
+                mplhep.cms.label(
+                    lumi=41.48,  # TODO: from self.config_inst.x.luminosity?
+                    com=13,
+                    ax=ax,
+                    llabel="Private Work",
+                    data=True,
+                )
+
                 text_eta_bin = eta_bin(eta_lo, eta_hi)
+                add_text(ax, pos_x, pos_y, text_eta_bin)
+                print(f"Start with eta {text_eta_bin} for {m} method")
+
+                plt.xlim(49, 2100)
+                ax.set_xscale("log")
+                plt.legend(loc="upper right")
+
                 store_bin_eta = f"eta_{dot_to_p(eta_lo)}_{dot_to_p(eta_hi)}"
+                self.output()["single"].child(
+                    f"jers_{m}_{store_bin_eta}.pdf",
+                    type="f",
+                ).dump(plt, formatter="mpl")
+                plt.close(fig)
