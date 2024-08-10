Fix JER calculation for FE method.

dijet/tasks/jer.py
@@ -131,8 +131,8 @@ class JER(
 
         # calculate JER for forward extension method
         # TODO: Check if factor 2 or 4. Keep consistent with UHH2 for now
-        jer_fe_val = np.sqrt(2 * h_widths[index_methods["fe"], :, :].values()**2 - jer_ref_val**2)
-        term_probe = 2 * h_widths[index_methods["fe"], :, :].values() * h_widths[index_methods["fe"], :, :].variances()
+        jer_fe_val = np.sqrt(4 * h_widths[index_methods["fe"], :, :].values()**2 - jer_ref_val**2)
+        term_probe = 4 * h_widths[index_methods["fe"], :, :].values() * h_widths[index_methods["fe"], :, :].variances()
         term_ref = jer_ref_val * jer_ref_err
         jer_fe_err = np.sqrt(term_probe**2 + term_ref**2) / jer_fe_val
 