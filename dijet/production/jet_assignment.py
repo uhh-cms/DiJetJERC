fa7ce2b23eff47993e30777046ee5926585f23e5
Daniel Savoiu <daniel.savoiu@cern.ch>
2025-10-10 15:33:10 +0200

    Refine `jet_assignment` producer.

--- dijet/production/jet_assignment.py
+++ dijet/production/jet_assignment.py
@@ -18,11 +18,6 @@ from dijet.constants import eta
 np = maybe_import("numpy")
 ak = maybe_import("awkward")
 
-"""
-Creates column probe and reference jets, used in dijet analysis.
-Only store pt, eta and phi field for now and remove mass and pdgId
-"""
-
 
 @producer(
     uses={
@@ -35,22 +30,40 @@ Only store pt, eta and phi field for now and remove mass and pdgId
     },
 )
 def jet_assignment(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
+    """
+    Producer to assign the probe and reference jets. Creates new four-vector
+    columns `probe_jet` and `reference_jet` as main result. Also produces
+    flags `use_sm` and `use_fe`, indicating whether the Standard Method (SM)
+    or Forward Extension (FE) method should be used for JER derivation later on.
+
+    The probe and reference jet assignment proceeds as follows:
+
+    If Jet[0] and Jet[1] are in the same |eta| bin, the SM is used and the jets
+    are assigned randomly as the probe and reference jets.
+
+    Otherwise, if either one of Jet[0] or Jet[1] lies in the central region
+    (|eta| < 1.131), it is selected as the reference jet, while the other one
+    is considered the probe jet. The FE method is used in this case.
+    """
 
     # TODO: for now, this only works for reco level
     jets = events.Jet
     events = set_ak_column(events, "n_jet", ak.num(jets.pt, axis=1), value_type=np.int32)
     jets = ak.pad_none(jets, 2)
 
-    eta_jet1 = np.abs(jets.eta[:, 0])
-    eta_jet2 = np.abs(jets.eta[:, 1])
-    # Check for Standard Method
-    eta_index_jet1 = np.digitize(ak.to_numpy(eta_jet1), eta) - 1
-    eta_index_jet2 = np.digitize(ak.to_numpy(eta_jet2), eta) - 1
-    use_sm = eta_index_jet1 == eta_index_jet2
-    events = set_ak_column(events, "use_sm", use_sm)
+    abseta_jet1 = np.abs(jets.eta[:, 0])
+    abseta_jet2 = np.abs(jets.eta[:, 1])
+
+    # check if two leading jets are in the same |eta| bin
+    abseta_index_jet1 = np.digitize(ak.to_numpy(abseta_jet1), eta) - 1
+    abseta_index_jet2 = np.digitize(ak.to_numpy(abseta_jet2), eta) - 1
+    jets_same_abseta_bin = (abseta_index_jet1 == abseta_index_jet2)
+
+    # set a flag for when the Standard Method should be used
+    events = set_ak_column(events, "use_sm", jets_same_abseta_bin)
 
     # use event numbers in chunk to seed random number generator
-    # TODO: use deterministic seeds!
+    # TODO: use deterministic seeds
     rand_gen = np.random.Generator(np.random.SFC64(events.event.to_list()))
     rand_ind = ak_random(
         ak.zeros_like(events.event),
@@ -58,31 +71,38 @@ def jet_assignment(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
         rand_func=rand_gen.integers,
     )
 
-    # Check for Forward extension
-    # We also use FE as a check when both jets are central
-    # TODO: not implemented yet!
-    jet1_is_central = eta_jet1 < 1.131
-    jet2_is_central = eta_jet2 < 1.131
-    both_central = jet1_is_central & jet2_is_central
+    # check whether leading jets are in the central detector region
+    jet1_is_central = (abseta_jet1 < 1.131)
+    jet2_is_central = (abseta_jet2 < 1.131)
+    both_jets_central = (jet1_is_central & jet2_is_central)
+    atleast_one_jet_central = (jet1_is_central | jet2_is_central)
+
+    # set a flag for when the Forward Extension should be used
+    # (at least one jet in the central region, except if if is in
+    # the same |eta| bin as the other jet -> SM)
     use_fe = (
-        (
-            jet1_is_central |  # at least one in barrel
-            jet2_is_central
-        ) &
-        ~use_sm  # not in the same eta bin
+        atleast_one_jet_central &
+        ~jets_same_abseta_bin
     )
     events = set_ak_column(events, "use_fe", use_fe)
 
-    # index of the central jet
-    # Use jet2 since true = 1 (subleading) and false = 0 (leading)
-    central_ind = ak.values_astype(jet2_is_central, np.uint8)
+    # determine index of central jet
+    idx_central_jet = ak.values_astype(
+        ak.where(
+            jet1_is_central,
+            0,  # leading jet is central jet -> index 0
+            1,  # subleading jet is central jet -> index 1
+        ),
+        np.uint8,
+    )
 
-    # if FE, choose the central jet as the reference jet, otherwise random
-    # if not SM and both central, also random
+    # compute index of reference and probe jets:
+    # - if one jet central and the other noncentral -> pick central jet as reference
+    # - if both or neither jet central -> pick randomly
     ref_index = ak.where(
-        use_fe & ~both_central,
-        central_ind,
-        rand_ind,  # if both central, random assignment
+        atleast_one_jet_central & ~both_jets_central,
+        idx_central_jet,
+        rand_ind,
     )
     pro_index = 1 - ref_index  # the opposite
 
