a55c995235ef6f0e62899229136cdb5e4b861440
Daniel Savoiu <daniel.savoiu@cern.ch>
2025-11-06 20:54:31 +0100

    New util functions: `product_dict`, `iter_bins`.

--- dijet/util.py
+++ dijet/util.py
@@ -57,3 +57,39 @@ def get_variable_for_level(config: od.Config, name: str, level: str):
         return var_inst.x("gen_variable", name)
     else:
         raise ValueError(f"invalid level '{level}', expected one of: gen,reco")
+
+
+def product_dict(input_dict: dict):
+    """
+    Generator that yields the Cartesian product of iterables in a dictionary.
+    Yielded elements are dictionaries that map keys of the original dictionary
+    to individual elements of the input sequences
+    """
+    from itertools import product
+
+    keys = input_dict.keys()
+    for instance in product(*input_dict.values()):
+        yield dict(zip(keys, instance))
+
+
+def iter_bins(bin_edges, **add_kwargs):
+    """
+    Generator that takes a sequence of (ascending) bin edges and yields a sequence
+    of dicts corresponding to individual bins. Dicts contain the following keys:
+    - *index*, the index of the current bin, starting from 0;
+    - *lo*, the lower edge of the bin;
+    - *hi*, the upper edge of the bin;
+    - any additional key-value pairs provided via *add_kwargs*.
+    """
+    # check reserved kwargs and raise
+    reserved_keys = set(add_kwargs).intersection({"index", "up", "dn"})
+    if reserved_keys:
+        reserved_keys_str = ",".join(sorted(reserved_keys))
+        raise ValueError(
+            "the following keys are reserved and cannot be present in add_kwargs: "
+            f"{reserved_keys_str}",
+        )
+
+    # yield bin dict
+    for i, (e_lo, e_hi) in enumerate(zip(bin_edges[:-1], bin_edges[1:])):
+        yield {"index": i, "lo": e_lo, "hi": e_hi, **add_kwargs}
